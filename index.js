a24e76489334c21b6c066a8fc4f57d2a65ec889d
jdu200027 <200027a@jdu.uz>
2024-12-26 23:14:24 +0500

    first commit

--- index.js
+++ index.js
@@ -1,37 +1,31 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    if (req.method === 'GET') {
+    const { statusCode } = res;
+    if (req.method === "GET" ) {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end(`
-            <h3>Status</h3>
-            <form action="/" method="POST">
-                <input name="email" type="email" placeholder="Input your email">
-                <button type="submit">Push</button>
-            </form>
-        `);
-    } else if (req.method === 'POST') {
-        let body = '';
-
-        // Collect data chunks
-        req.on('data', chunk => {
-            body += chunk.toString();
+                </h1>Status</h4>
+                <form action="/" method="POST">
+                    <input name="username">
+                    <button type="submit">BTN</button> 
+                </form>
+            `)
+    } else if (req.method === "POST" ) {
+        const result = [];
+        req.on('data', attr => {
+            result.push(Buffer.from(attr))
         });
 
-        // When all data is received
         req.on('end', () => {
-            // Parse form-encoded data (e.g., email=value)
-            const params = new URLSearchParams(body);
-            const email = params.get('email'); // Extract the input value by its name attribute
-
-            console.log('Email:', email);
-
-            res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end(`Received email: ${email}`);
-        });
+            const message = result.toString()
+            res.end(
+                `
+                <p>Email is ${message}</p>
+            `
+            )
+        })
     }
-});
+})
 
-server.listen(8080, () => {
-    console.log('Server running on port 8080');
-});
+server.listen(5000, console.log("Server is running 5000"));
